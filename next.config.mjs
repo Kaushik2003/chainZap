0b922c24683378e02353847982a61c6c8b3068aa
Debanjannn <debanjanspider5@gmail.com>
2025-07-05 13:42:28 +0530

    updated config

--- next.config.mjs
+++ next.config.mjs
@@ -9,4 +9,7 @@ export default withPWA({
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   basePath: process.env.NEXT_PUBLIC_BASE_PATH, // Sets the base path to `/some-base-path`.
+  images: {
+    unoptimized: true,
+  },
 });
